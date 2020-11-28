8fb10733a13ed757b7e695f95fb47a776e1b07e3
gabbs <gortman@rednovalabs.com>
2020-11-27 18:48:19 -0600

    add react-spring example for a nice little flipping effect :)

--- src/components/Polaroid/Polaroid.js
+++ src/components/Polaroid/Polaroid.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring'
 import styles from './styles.scss';
 
-const Polaroid = ({ source, caption }) => (
-  <div className={styles.root}>
-    <div className={styles.container}>
+// codepen flip taken from: https://codesandbox.io/embed/01yl7knw70
+const Polaroid = ({ source, caption, alt }) => {
+  const [flipped, setFlipped] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? -180 : 0}deg)`,
+    config: { mass: 5, tension: 250, friction: 50 }
+  });
+
+  const front = (
+    <animated.div
+      className={styles.container}
+      style={{ opacity: opacity.interpolate(o => 1 - o), transform }}
+    >
+    <div className={styles.body}>
+      <img
+        src={source}
+        alt={alt}
+      />
+      <span>{caption}</span>
+    </div>
+    </animated.div>
+  );
+
+  const back = (
+    <animated.div
+      className={styles.container}
+      style={{ opacity, transform: transform.interpolate(t => `${t} rotateY(-180deg)`) }}
+    >
       <div className={styles.body}>
-        <img
-          src={source}
-          alt="rabbit"
-        />
         <span>{caption}</span>
       </div>
+    </animated.div>
+  );
+
+  return (
+    <div
+      className={styles.root}
+      onClick={() => setFlipped(state => !state)}
+    >
+      {front}
+      {back}
     </div>
-  </div>
-);
+  );
+};
 
 export default Polaroid;
