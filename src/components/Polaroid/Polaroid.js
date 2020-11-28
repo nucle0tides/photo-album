9380efd2cff7d72caef19c5cd51e23f70aedd093
gabbs <gortman@rednovalabs.com>
2020-11-27 22:03:15 -0600

    start attempting to get exif data on image load

--- src/components/Polaroid/Polaroid.js
+++ src/components/Polaroid/Polaroid.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring'
+import React, { useState, useEffect, useRef } from 'react';
+import { useSpring, animated } from 'react-spring';
+import EXIF from 'exif-js';
 import styles from './styles.scss';
 
 // codepen flip taken from: https://codesandbox.io/embed/01yl7knw70
 const Polaroid = ({ source, caption, alt }) => {
+  const [date, setDate] = useState(undefined);
   const [flipped, setFlipped] = useState(false);
+  const imgRef = useRef();
   const { transform, opacity } = useSpring({
     opacity: flipped ? 1 : 0,
     transform: `perspective(600px) rotateY(${flipped ? -180 : 0}deg)`,
     config: { mass: 5, tension: 250, friction: 50 }
   });
+
+  React.useEffect(() => {
+    if (imgRef && imgRef.current) {
+      imgRef.current.addEventListener('load', imageLoad);
+      return () => imgRef.current.removeEventListener('load', imageLoad);
+    }
+  }, [imgRef]);
+
+  const imageLoad = (e) => {
+    const source = e.target;
+    EXIF.getData(source, function() {
+      console.log(EXIF.getTag(this, "DateTimeOriginal"));
+    });
+  }
 
   const front = (
     <animated.div
@@ -21,6 +38,7 @@ const Polaroid = ({ source, caption, alt }) => {
         <img
           src={source}
           alt={alt}
+          ref={imgRef}
         />
       </div>
       <span>{caption}</span>
